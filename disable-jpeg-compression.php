bf517a6794c30b50b17ba7dd9d16dca627356de9
XxXYonIXxX <XxXYonIXxX@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-04 18:44:04 +0000

    Added basic security
    git-svn-id: https://plugins.svn.wordpress.org/disable-jpeg-compression/trunk@907842 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- disable-jpeg-compression.php
+++ disable-jpeg-compression.php
@@ -1,12 +1,14 @@
 <?php
-/*/
+/*
 Plugin Name: Disable JPEG Compression
 Plugin URI:
 Description: Disable the JPEG compression in WordPress, which enabled by default.
-Version: 0.1
+Version: 0.2
 Author: Yonatan Ganot
 Author URI: http://www.scolpy.net
-/*/
+*/
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 add_filter('jpeg_quality', create_function('', 'return 100;'));
 ?>
--- readme.txt
+++ readme.txt
@@ -4,6 +4,6 @@ Donate link: http://www.scolpy.net/
 Tags: jpeg, jpg, compression, disable
 Requires at least: 3.3.1
 Tested up to: 3.9
-Stable tag: 0.1
+Stable tag: 0.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
