6fe29e584de203968bcdafa9d4e594391fbac334
XxXYonIXxX <XxXYonIXxX@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-05 18:25:39 +0000

    Added backward compatibility to PHP 5.2 and lower
    git-svn-id: https://plugins.svn.wordpress.org/disable-jpeg-compression/trunk@1060618 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- disable-jpeg-compression.php
+++ disable-jpeg-compression.php
@@ -10,14 +10,6 @@ Author URI: http://www.scolpy.net
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-if (version_compare(phpversion(), '5.2', '>'))
-{
-add_filter('jpeg_quality', function($arg){return 100;});
-add_filter('wp_editor_set_quality', function($arg){return 100;});
-}
-else
-{
 add_filter('jpeg_quality', create_function('', 'return 100;'));
 add_filter('wp_editor_set_quality', create_function('', 'return 100;'));
-}
 ?>
